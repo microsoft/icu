8e747afbe0c19c1b06ccfa30ffee7058b5b64d61
Mitchell Hwang <mitchhwang1418@gmail.com>
2021-02-23 13:15:57 -0500

    Add armv7 to iOS icu build (#86)
    Co-authored-by: Mitchell Hwang <mitchell.hwang@microsoft.com>

--- eng/icu.ios.mk
+++ eng/icu.ios.mk
@@ -1,22 +1,22 @@
 IOS_MIN_VERSION=8.0
 
 ifeq ($(TARGET_ARCHITECTURE),x64)
-	IOS_ARCH=x86_64
+	IOS_ARCH=-arch x86_64
 	IOS_SDK=iphonesimulator
 	IOS_ICU_HOST=i686-apple-darwin11
 endif
 ifeq ($(TARGET_ARCHITECTURE),x86)
-	IOS_ARCH=i386
+	IOS_ARCH=-arch i386
 	IOS_SDK=iphonesimulator
 	IOS_ICU_HOST=i686-apple-darwin11
 endif
 ifeq ($(TARGET_ARCHITECTURE),arm64)
-	IOS_ARCH=arm64
+	IOS_ARCH=-arch arm64
 	IOS_SDK=iphoneos
 	IOS_ICU_HOST=arm-apple-darwin
 endif
 ifeq ($(TARGET_ARCHITECTURE),arm)
-	IOS_ARCH=armv7s
+	IOS_ARCH=-arch armv7 -arch armv7s
 	IOS_SDK=iphoneos
 	IOS_ICU_HOST=arm-apple-darwin
 endif
@@ -25,8 +25,8 @@ XCODE_DEVELOPER := $(shell xcode-select --print-path)
 XCODE_SDK := $(shell xcodebuild -version -sdk $(IOS_SDK) | grep -E '^Path' | sed 's/Path: //')
 
 CONFIGURE_COMPILER_FLAGS += \
-	CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(ICU_DEFINES) -isysroot $(XCODE_SDK) -I$(XCODE_SDK)/usr/include/ -arch $(IOS_ARCH) -miphoneos-version-min=$(IOS_MIN_VERSION)" \
-	CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(ICU_DEFINES) -isysroot $(XCODE_SDK) -I$(XCODE_SDK)/usr/include/ -I./include/ -arch $(IOS_ARCH) -miphoneos-version-min=$(IOS_MIN_VERSION)" \
+	CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(ICU_DEFINES) -isysroot $(XCODE_SDK) -I$(XCODE_SDK)/usr/include/ $(IOS_ARCH) -miphoneos-version-min=$(IOS_MIN_VERSION)" \
+	CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(ICU_DEFINES) -isysroot $(XCODE_SDK) -I$(XCODE_SDK)/usr/include/ -I./include/ $(IOS_ARCH) -miphoneos-version-min=$(IOS_MIN_VERSION)" \
 	LDFLAGS="-L$(XCODE_SDK)/usr/lib/ -isysroot $(XCODE_SDK) -miphoneos-version-min=$(IOS_MIN_VERSION)" \
 	CC="$(XCODE_DEVELOPER)/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" \
 	CXX="$(XCODE_DEVELOPER)/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++"
