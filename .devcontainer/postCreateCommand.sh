25193870ad2c64ae466303824e545709986df2da
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2022-08-31 11:17:35 +0000

    Building ICU in container + required setup for it.

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+EMSDK_PATH=$PWD/artifacts/emsdk
+rm -rf $EMSDK_PATH
+git clone https://github.com/emscripten-core/emsdk.git $EMSDK_PATH
+EMSCRIPTEN_VERSION="`cat ./.devcontainer/emscripten-version.txt 2>&1`"
+cd $EMSDK_PATH && ./emsdk install $EMSCRIPTEN_VERSION
+cd $EMSDK_PATH && ./emsdk activate $EMSCRIPTEN_VERSION
+cd ../../
+export EMSDK_PATH=$EMSDK_PATH
+./build.sh /p:TargetOS=Browser /p:TargetArchitecture=wasm /p:IcuTracing=true
