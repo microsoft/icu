731657f7ae689b76951d1b103e74aa4f06ab2f49
Alexander Köplinger <alex.koeplinger@outlook.com>
2023-02-16 20:15:14 +0100

    Bump iOS/tvOS min version to 11, remove 32bit targets and remove bitcode from tvOS (#318)
    * Bump iOS min version to 11, remove 32bit targets and remove bitcode from tvOS
    
    We did the same in dotnet/runtime with https://github.com/dotnet/runtime/pull/79349 and https://github.com/dotnet/runtime/pull/81965
    
    * Fixup

--- eng/icu.tvossimulator.mk
+++ eng/icu.tvossimulator.mk
@@ -1,9 +1,9 @@
-TVOS_MIN_VERSION=10.0
+TVOS_MIN_VERSION=11.0
 
 ifeq ($(TARGET_ARCHITECTURE),x64)
 	TVOS_ARCH=x86_64
 	TVOS_SDK=appletvsimulator
-	TVOS_ICU_HOST=i686-apple-darwin
+	TVOS_ICU_HOST=x86_64-apple-darwin
 endif
 ifeq ($(TARGET_ARCHITECTURE),arm64)
 	TVOS_ARCH=arm64
@@ -15,9 +15,9 @@ XCODE_DEVELOPER := $(shell xcode-select --print-path)
 XCODE_SDK := $(shell xcodebuild -version -sdk $(TVOS_SDK) | grep -E '^Path' | sed 's/Path: //')
 
 CONFIGURE_COMPILER_FLAGS += \
-	CFLAGS="-fembed-bitcode -Oz -fno-exceptions -Wno-sign-compare $(ICU_DEFINES) -isysroot $(XCODE_SDK) -I$(XCODE_SDK)/usr/include/ -arch $(TVOS_ARCH) -mappletvsimulator-version-min=$(TVOS_MIN_VERSION)" \
-	CXXFLAGS="-fembed-bitcode -Oz -fno-exceptions -Wno-sign-compare $(ICU_DEFINES) -isysroot $(XCODE_SDK) -I$(XCODE_SDK)/usr/include/ -I./include/ -arch $(TVOS_ARCH) -mappletvsimulator-version-min=$(TVOS_MIN_VERSION)" \
-	LDFLAGS="-fembed-bitcode -L$(XCODE_SDK)/usr/lib/ -isysroot $(XCODE_SDK) -mappletvsimulator-version-min=$(TVOS_MIN_VERSION)" \
+	CFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(ICU_DEFINES) -isysroot $(XCODE_SDK) -I$(XCODE_SDK)/usr/include/ -arch $(TVOS_ARCH) -mappletvsimulator-version-min=$(TVOS_MIN_VERSION)" \
+	CXXFLAGS="-Oz -fno-exceptions -Wno-sign-compare $(ICU_DEFINES) -isysroot $(XCODE_SDK) -I$(XCODE_SDK)/usr/include/ -I./include/ -arch $(TVOS_ARCH) -mappletvsimulator-version-min=$(TVOS_MIN_VERSION)" \
+	LDFLAGS="-L$(XCODE_SDK)/usr/lib/ -isysroot $(XCODE_SDK) -mappletvsimulator-version-min=$(TVOS_MIN_VERSION)" \
 	CC="$(XCODE_DEVELOPER)/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" \
 	CXX="$(XCODE_DEVELOPER)/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++"
 
