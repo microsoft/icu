87f8d88bfaf0e71c4664388e213a92cbde97190c
Rahul Pandey <103115900+rp9-next@users.noreply.github.com>
2023-07-14 15:29:01 +0530

    Update changelog and version number to 72.1.0.1

--- icu/icu4c/source/common/unicode/uvernum.h
+++ icu/icu4c/source/common/unicode/uvernum.h
@@ -72,7 +72,7 @@
  *  @stable ICU 4.0
  */
 #ifndef U_ICU_VERSION_BUILDLEVEL_NUM
-#define U_ICU_VERSION_BUILDLEVEL_NUM 0
+#define U_ICU_VERSION_BUILDLEVEL_NUM 1
 #endif
 
 /** Glued version suffix for renamers
@@ -132,7 +132,7 @@
  *  This value will change in the subsequent releases of ICU
  *  @stable ICU 2.4
  */
-#define U_ICU_VERSION "72.1"
+#define U_ICU_VERSION "72.1.0.1"
 
 /**
  * The current ICU library major version number as a string, for library name suffixes.
@@ -151,7 +151,7 @@
 /** Data version in ICU4C.
  * @internal ICU 4.4 Internal Use Only
  **/
-#define U_ICU_DATA_VERSION "72.1"
+#define U_ICU_DATA_VERSION "72.1.0.1"
 #endif  /* U_HIDE_INTERNAL_API */
 
 /*===========================================================================
