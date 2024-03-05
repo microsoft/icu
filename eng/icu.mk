9712d1680642c09dfd46827f5e850b9027ba6086
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-03-05 16:10:33 +0100

    Only use lld linker if it is available (#430)

--- eng/icu.mk
+++ eng/icu.mk
@@ -45,7 +45,11 @@ UNAME_S := $(shell uname -s)
 LINKER_OVERRIDE = 
 ifeq ($(UNAME_S),Linux)
 ifeq ($(TARGET_OS),browser)
-	LINKER_OVERRIDE = LDFLAGS=-fuse-ld=lld
+	# Check if lld is available
+	LLD_EXISTS := $(shell command -v lld 2> /dev/null)
+	ifneq ($(LLD_EXISTS),)
+		LINKER_OVERRIDE = LDFLAGS=-fuse-ld=lld
+	endif
 endif
 endif
 
